f350387a89eddb973cca4a779ac4cdbb5d11a19a
Dylan Valkenaar <valkdylan@gmail.com>
2024-02-13 11:51:11 +0100

    Added player class

---
+++ GXPEngine/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;                                // GXPEngine contains the engine
+using System.Drawing;
+using System.Media;
+
+namespace GXPEngine
+{
+    public class Player : AnimationSprite
+    {
+
+       private string filename;
+       private int cols;
+       private int rows;
+       private int frames;
+       private bool addCollider;
+
+       public Player(string filename, int cols, int rows, int frames = -1, bool addCollider = true) : base(filename, cols, rows, frames, addCollider)
+        {
+            this.filename = filename;
+            this.cols = cols;
+            this.rows = rows;
+            this.frames = frames;
+
+            this.addCollider = addCollider;
+        }
+    }
+}
